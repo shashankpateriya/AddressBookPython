83c00f74ca9e98e2679a04845d6535d985affc2d
Shashank <shashankpateriya4@gmail.com>
2021-09-21 13:29:21 +0530

    [shashank] Add: Method to create contact

--- contacts.py
+++ contacts.py
@@ -9,6 +9,9 @@ class Contact:
         self.phone_number = phone_number
         self.email = email
 
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
     def __str__(self):
         return f"First name : {self.first_name} \nLast name : {self.last_name} \nAddress : {self.address} \n" \
                f"City : {self.city} \nState : {self.state} \nZip : {self.zip} \n" \
