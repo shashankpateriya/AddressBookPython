3fae40ea3fa990359c8ca716241ae25e6bfe8c85
Shashank <shashankpateriya4@gmail.com>
2021-09-21 13:29:56 +0530

    [shashank] Add: Editing contact in address book

--- address_book.py
+++ address_book.py
@@ -1,10 +1,12 @@
 from contacts import Contact
+
 if __name__ == '__main__':
     contacts = []
     user_input = ""
     while user_input != "q":
         print("1 - Enter a contact")
         print("2 - Display contacts")
+        print("3 - Edit a contact")
         print("q - quit program")
         user_input = input("Select option: ")
         if user_input == "1":
@@ -22,5 +24,20 @@ if __name__ == '__main__':
         elif user_input == "2":
             for contact in contacts:
                 print(contact)
-        else:
-            break
+        elif user_input == "3":
+            to_lookup = input("Enter contact's name to lookup\n")
+            for contact in contacts:
+                if to_lookup in contact.full_name():
+                    new_first_name = input("Enter first name \n")
+                    new_last_name = input("Enter last name \n")
+                    new_address = input("Enter address \n")
+                    new_city = input("Enter city \n")
+                    new_state = input("Enter state \n")
+                    new_zip = input("Enter zip code \n")
+                    new_phone_number = input("Enter phone number \n")
+                    new_email = input("Enter email address \n")
+                    new_contact = Contact(new_first_name, new_last_name, new_address, new_city, new_state,
+                                      new_zip, new_phone_number, new_email)
+                    contacts.append(new_contact)
+                else:
+                    break
