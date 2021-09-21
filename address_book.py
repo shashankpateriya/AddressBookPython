c48586339f496362ba608a75978a29e250eb0378
Shashank <shashankpateriya4@gmail.com>
2021-09-21 16:09:12 +0530

    [shashank] Add: Adding multiple contact in address book

--- address_book.py
+++ address_book.py
@@ -1,4 +1,15 @@
+import logging
+import pandas
 from contacts import Contact
+
+logging.basicConfig(
+    filename="addressbooklogger.log",
+    filemode="a",
+    format="%(asctime)s %(levelname)s-%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+log = logging.getLogger()
+
 
 if __name__ == '__main__':
     contacts = []
@@ -7,42 +18,53 @@ if __name__ == '__main__':
         print("1 - Enter a contact")
         print("2 - Display contacts")
         print("3 - Edit a contact")
+        print("4 - Delete a contact")
         print("q - quit program")
         user_input = input("Select option: ")
-        if user_input == "1":
-            print("Enter contact information")
-            first_name = input("Enter first name \n")
-            last_name = input("Enter last name \n")
-            address = input("Enter address \n")
-            city = input("Enter city \n")
-            state = input("Enter state \n")
-            zip = input("Enter zip code \n")
-            phone_number = input("Enter phone number \n")
-            email = input("Enter email address \n")
-            our_contact = Contact(first_name, last_name, address, city, state, zip, phone_number, email)
-            contacts.append(our_contact)
-        elif user_input == "2":
+        try:
+            if user_input == "1":
+                print("Enter contact information")
+                first_name = input("Enter first name \n")
+                last_name = input("Enter last name \n")
+                address = input("Enter address \n")
+                city = input("Enter city \n")
+                state = input("Enter state \n")
+                zip = input("Enter zip code \n")
+                phone_number = input("Enter phone number \n")
+                email = input("Enter email address \n")
+
+                our_contact = Contact(first_name, last_name, address, city, state, zip, phone_number, email)
+                contacts.append(our_contact)
+            elif user_input == "2":
+                for contact in contacts:
+                    print(contact)
+            elif user_input == "3":
+                to_lookup = input("Enter contact's full name to delete\n")
+                for contact in contacts:
+                    if to_lookup in contact.full_name():
+                        first_name = input("Enter new first name \n")
+                        last_name = input("Enter new last name \n")
+                        address = input("Enter new address \n")
+                        city = input("Enter new city \n")
+                        state = input("Enter new state \n")
+                        zip = input("Enter new zip code \n")
+                        phone_number = input("Enter new phone number \n")
+                        email = input("Enter new email address \n")
+                        new_contact = Contact(first_name, last_name, address, city, state, zip, phone_number, email)
+                        contacts.append(new_contact)
+            elif user_input == "4":
+                to_delete = input("Enter contact's name to lookup\n")
+                for contact in contacts:
+                    if to_delete in contact.full_name():
+                        contacts.remove(contact)
+            elif user_input == "":
+                log.warning("No input was given")
+            elif user_input == "q":
+                break
+        except Exception as e:
+            raise ValueError
+
+        with open("contactsdata.csv", "w") as f:
             for contact in contacts:
-                print(contact)
-        elif user_input == "3":
-            to_lookup = input("Enter contact's name to lookup\n")
-            for contact in contacts:
-                if to_lookup in contact.full_name():
-                    new_first_name = input("Enter first name \n")
-                    new_last_name = input("Enter last name \n")
-                    new_address = input("Enter address \n")
-                    new_city = input("Enter city \n")
-                    new_state = input("Enter state \n")
-                    new_zip = input("Enter zip code \n")
-                    new_phone_number = input("Enter phone number \n")
-                    new_email = input("Enter email address \n")
-                    new_contact = Contact(new_first_name, new_last_name, new_address, new_city, new_state,
-                                      new_zip, new_phone_number, new_email)
-                    contacts.append(new_contact)
-        elif user_input == "4":
-            to_delete = input("Enter contact's name to lookup\n")
-            for contact in contacts:
-                if to_delete in contact.full_name():
-                    contacts.remove(contact)
-        else:
-            break
+                f.write(f"{contact.first_name},{contact.last_name},{contact.address},{contact.city},{contact.state}"
+                        f"{contact.zip},{contact.phone_number},{contact.email}\n")
